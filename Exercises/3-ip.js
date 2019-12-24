29b63ca967620ab2b50c00de3e9a7fcc5e2729bc
Yaroslav R <rostanets@gmail.com>
2019-12-24 19:05:49 +0200

    upd

--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  const fn = (res, item) => (res << 8) + parseInt(item);
-  return ip.split('.').reduce(fn, 0);
+  const fn = (cur, i) => {while(i) {cur = cur << 8; i--} return cur};
+  return ip.split('.').reverse().reduce((acc, c, i) => acc += fn(+c, i), 0)
 };
+
+console.log(ipToInt());
+
 module.exports = { ipToInt };
